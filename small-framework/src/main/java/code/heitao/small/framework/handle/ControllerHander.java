1ea6d1534f78e539f071aae920f791b69846e8d0
Aimintao <wb_taoaimin@zhongan.com>
2018-06-22 17:33:53 +0800

    新增ControllerHander

---
+++ small-framework/src/main/java/code/heitao/small/framework/handle/ControllerHander.java
@@ -0,0 +1,11 @@
+package code.heitao.small.framework.handle;
+
+/**
+ * @author Aimin
+ * @Title: ControllerHander
+ * @ProjectName lightFrame架构
+ * @Description: TODO
+ * @date 2018/6/22 17:30
+ */
+public class ControllerHander {
+}
