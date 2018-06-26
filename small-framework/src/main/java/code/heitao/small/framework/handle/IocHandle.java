2728b88cb0519b74765cc6cc92fd47d512923083
Aimintao <wb_taoaimin@zhongan.com>
2018-06-26 11:59:07 +0800

    add ArrayUtil判断数组Fielf[]是否为空

--- small-framework/src/main/java/code/heitao/small/framework/handle/IocHandle.java
+++ small-framework/src/main/java/code/heitao/small/framework/handle/IocHandle.java
@@ -1,6 +1,7 @@
 package code.heitao.small.framework.handle;
 
 import code.heitao.small.framework.annotation.Inject;
+import code.heitao.small.framework.util.ArrayUtil;
 import code.heitao.small.framework.util.CollectionUtil;
 import code.heitao.small.framework.util.ReflectionUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -24,23 +25,25 @@ public class IocHandle {
         if (CollectionUtil.isNotEmpty(beanMap)) {
             //循环从beanMap中获取bean类和对应的实例，并获取bean类的field
             // 判断如果含有inject注解就setfield反射注入到实例中
-            for (Map.Entry<Class<?>,Object> beanEntry:beanMap.entrySet()
-                 ){
-                Class<?>  beanEntryKey= beanEntry.getKey();
+            for (Map.Entry<Class<?>, Object> beanEntry : beanMap.entrySet()
+                    ) {
+                Class<?> beanEntryKey = beanEntry.getKey();
                 Object beanEntryValue = beanEntry.getValue();
                 //获取bean类中的所有field
                 Field[] declaredFields = beanEntryKey.getDeclaredFields();
-                for (Field field:declaredFields
-                     ) {
-                    //判断field中是否含有inject注解
-                    if (field.isAnnotationPresent(Inject.class)){
-                        //获取field对应的class
-                        Class<?> fieldClass = field.getType();
-                        //获取实例
-                        Object beanFieldClass = beanMap.get(fieldClass);
-                        if (beanFieldClass!=null){
-                            //set注入到beanEntryKey类的field中
-                            ReflectionUtil.setField(beanEntryKey,field,beanFieldClass);
+                if (ArrayUtil.isNotEmpty(declaredFields)) {
+                    for (Field field : declaredFields
+                            ) {
+                        //判断field中是否含有inject注解
+                        if (field.isAnnotationPresent(Inject.class)) {
+                            //获取field对应的class
+                            Class<?> fieldClass = field.getType();
+                            //获取实例
+                            Object beanFieldClass = beanMap.get(fieldClass);
+                            if (beanFieldClass != null) {
+                                //set注入到beanEntryKey类的field中
+                                ReflectionUtil.setField(beanEntryKey, field, beanFieldClass);
+                            }
                         }
                     }
                 }
